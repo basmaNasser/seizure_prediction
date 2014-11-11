ab1c2bb242ad7a59bdbc013444354bceec367b03
Michael Mortonson <michael.mortonson@gmail.com>
2014-11-10 17:41:40 -0800

    Add functions to compute basic features

---
+++ features.py
@@ -0,0 +1,30 @@
+# Define functions to compute features from the EEG time series data.
+
+import numpy as np
+
+
+def mean_of_means(data):
+    return np.mean(data)
+
+def mean_of_stds(data):
+    return np.mean(np.std(data, axis=1))
+
+def std_of_stds(data):
+    return np.std(np.std(data, axis=1))
+
+def max_dev(data):
+    return np.max(np.abs(data.T - f_mean(data)))
+
+def compute_features(data, functions, labels):
+    """
+    Given a matrix with EEG voltage time series in rows
+    (one row for each electrode recorded in a particular segment),
+    and a dictionary of functions to apply to each time series, return
+    an array with the resulting features and a list of column labels.
+    """
+    features = []
+    columns = []
+    for f, col in zip(functions, labels):
+        features.append(f(data))
+        columns.append(col)
+    return (features, columns)
