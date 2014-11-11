53a8daa3bc87d985fdd4fc71cc83cd74f87fdcdd
Michael Mortonson <michael.mortonson@gmail.com>
2014-11-10 16:07:20 -0800

    Add script to plot time series and power spectra for random examples

---
+++ plot_random_segments.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Randomly draw one preictal segment and one interictal segment
+# and, for each, plot the time series and power spectrum from a 
+# single, randomly-chosen electrode.
+
+import os.path
+import numpy as np
+import matplotlib.pyplot as plt
+import load_data
+
+data_dir = os.path.abspath('data/Dog_1')
+
+pre_data, inter_data = [load_data.load_random_data(data_dir, seg_type,
+                                                   print_file_name=True) for \
+                            seg_type in ('preictal', 'interictal')]
+
+pre_shape = pre_data['data'].shape
+pre_elec = np.random.choice(pre_shape[0])
+inter_shape = inter_data['data'].shape
+inter_elec = np.random.choice(inter_shape[0])
+
+fig = plt.figure(figsize=(8,8))
+fig.set_tight_layout(True)
+
+plt.subplot(221)
+voltage = pre_data['data'][pre_elec,:]
+plt.plot(np.linspace(0, pre_data['length_sec'], num=pre_shape[1]),
+         voltage, 'k-')
+plt.xlabel('time [s]')
+plt.ylabel('preictal voltage')
+
+plt.subplot(222)
+freq = np.fft.rfftfreq(voltage.size, d=1./pre_data['sampling_rate_hz'])
+power = np.abs(np.fft.rfft(voltage))**2
+plt.loglog(freq, power, 'k-')
+plt.xlabel('frequency [Hz]')
+plt.ylabel('preictal power')
+
+plt.subplot(223)
+voltage = inter_data['data'][inter_elec,:]
+plt.plot(np.linspace(0, inter_data['length_sec'], num=inter_shape[1]),
+         voltage, 'k-')
+plt.xlabel('time [s]')
+plt.ylabel('interictal voltage')
+
+plt.subplot(224)
+freq = np.fft.rfftfreq(voltage.size, d=1./inter_data['sampling_rate_hz'])
+power = np.abs(np.fft.rfft(voltage))**2
+plt.loglog(freq, power, 'k-')
+plt.xlabel('frequency [Hz]')
+plt.ylabel('interictal power')
+
+plt.show()
