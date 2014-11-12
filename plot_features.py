75ba85840419168bafa61a874e462d9447307e84
Michael Mortonson <michael.mortonson@gmail.com>
2014-11-12 15:48:30 -0800

    Add power spectrum features to plot (and some extra options)

--- plot_features.py
+++ plot_features.py
@@ -9,51 +9,86 @@ import matplotlib.pyplot as plt
 import features
 
 data_dir = os.path.abspath('data/Dog_1')
-feature_file = os.path.join(data_dir, 'features_01.txt')
-
-# set up plot
-fig = plt.figure(figsize=(8,8))
-fig.set_tight_layout(True)
+feature_file = os.path.join(data_dir, 'features_02.txt')
 
 # define functions and labels for features
 feature_functions = [features.mean_of_means,
                      features.std_of_means,
                      features.mean_of_stds,
                      features.std_of_stds,
-                     features.max_dev]
-feature_labels = ['mean',
-                  'std. dev. of means',
-                  'mean of std. devs.',
-                  'std. dev. of std. devs.',
-                  'max dev.']
+                     features.max_dev,
+                     features.power_mean,
+                     features.power_cov]
+feature_labels = ['MM', 'SDM', 'MSD', 'SDSD', 'MaxD'] + \
+                 ['PM' + str(i) for i in range(1, 6)] + \
+                 ['PC' + str(i) + str(j) for i in range(1,6) \
+                                         for j in range(i,6)]
+
+# choose features to include in the plots
+i_feature = np.array(range(len(feature_labels)))
+#
+#use_features = (i_feature < 5)
+#use_features = (i_feature > 4) & (i_feature < 10)
+use_features = (i_feature > 9)
+#
+feature_labels_used = list(np.array(feature_labels)[use_features])
+n_features = len(feature_labels_used)
+
+# fraction of interictal outliers to exclude in plot ranges
+# - set to None to show all points
+plot_outlier_fraction = 0.05
+# extra fraction of plot to show (not used if plot_outlier_fraction is None)
+plot_f_edge = 0.05
 
 # compute and save features, or laod previously computed features
 if os.path.isfile(feature_file):
     X = np.loadtxt(feature_file)
 else:
+    print 'Computing features...'
     X, data_files = features.compute_feature_matrix(data_dir,
                                             feature_functions, feature_labels,
-                                            save_file=feature_file)
+                                            save_file=feature_file,
+                                            verbose=True)
 
 X = features.scale_features(X)
 
 # plot features for each segment
+fig, axs = plt.subplots(n_features-1, n_features-1, figsize=(9,9),
+                        sharex='col', sharey='row')
+plt.subplots_adjust(hspace=0.001, wspace=0.001)
+for i in range(n_features-1):
+    for j in range(i+1, n_features-1):
+        fig.delaxes(axs[i,j])
+        
 for seg_type, color, marker in zip([0, 1],
                                    ['k', 'r'],
                                    ['.', 'x']):
     # select rows matching a given segment type and remove (hour, type) cols.
     seg_features = X[X[:,1] == seg_type, 2:]
 
-    n_features = len(feature_functions)
-    for i in range(1,n_features):
+    # select features to show in plot
+    seg_features_used = seg_features[:,use_features]
+
+    for i in range(1, n_features):
         for j in range(i):
-            plt.subplot(n_features-1, n_features-1, (n_features-1)*(i-1)+j+1)
-            plt.scatter(seg_features[:,j], seg_features[:,i],
-                        c=color, marker=marker, s=20)
-            if i == len(feature_functions)-1:
-                plt.xlabel(feature_labels[j])
+            ax = axs[i-1, j]
+            fx = seg_features_used[:,j]
+            fy = seg_features_used[:,i]
+            ax.scatter(fx, fy, c=color, marker=marker, s=20)
+            if seg_type == 0 and plot_outlier_fraction is not None:
+                i_min = int(plot_outlier_fraction*len(fx))
+                i_max = int((1.-plot_outlier_fraction)*len(fx))
+                fx_cut = np.sort(fx)[i_min:i_max]
+                fy_cut = np.sort(fy)[i_min:i_max]
+                fx_ext, fy_ext = [np.max(v)-np.min(v) for v in (fx_cut, fy_cut)]
+                ax.set_xlim((np.min(fx_cut) - plot_f_edge*fx_ext,
+                             np.max(fx_cut) + plot_f_edge*fx_ext))
+                ax.set_ylim((np.min(fy_cut) - plot_f_edge*fy_ext,
+                             np.max(fy_cut) + plot_f_edge*fy_ext))
+            if i == n_features-1:
+                ax.set_xlabel(feature_labels_used[j])
             if j == 0:
-                plt.ylabel(feature_labels[i])
+                ax.set_ylabel(feature_labels_used[i])
 
 plt.show()
     
