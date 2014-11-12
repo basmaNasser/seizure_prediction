7400b9734a473da01d6b9f80ac989ad2f81d6240
Michael Mortonson <michael.mortonson@gmail.com>
2014-11-11 18:48:23 -0800

    Add learning curve plots to analysis script

--- train_logistic.py
+++ train_logistic.py
@@ -12,10 +12,11 @@ import features
 
 features_file = os.path.abspath('data/Dog_1/features_01.txt')
 type_column = 1    # column listing segment type
-feature_columns = [5]    # columns to include in model
+feature_columns = [4, 5]    # columns to include in model
 n_cv = 10    # number of CV iterations
 n_pre_hrs = 1    # number of 6-segment preictal clips to use in CV samples
-C_reg = 1.0    # inverse of regularization strength
+n_learning_curve = 20    # number of steps for learning curves
+C_reg = 0.001    # inverse of regularization strength
 
 X = np.loadtxt(features_file)
 X = features.scale_features(X)
@@ -24,10 +25,12 @@ model = linear_model.LogisticRegression(C=C_reg, class_weight='auto')
 
 auc_values = []
 
-# set up ROC plot
-fig = plt.figure(figsize=(6,4))
+# set up learning curve and ROC plots
+fig = plt.figure(figsize=(8,4))
 fig.set_tight_layout(True)
-plt.plot(np.linspace(0, 1), np.linspace(0, 1), 'k:')
+ax0 = plt.subplot(121)
+ax1 = plt.subplot(122)
+ax1.plot(np.linspace(0, 1), np.linspace(0, 1), 'k:')
 
 # loop over random training-CV sample splittings
 for i_cv in range(n_cv):
@@ -45,7 +48,29 @@ for i_cv in range(n_cv):
     pct_train = 100.*len(train_class)/float(len(train_class)+len(cv_class))
     print len(train_class), 'training instances ({0:.1f}%)'.format(pct_train)
     print len(cv_class), 'CV instances ({0:.1f}%)'.format(100.-pct_train)
-                             
+
+    # loop over fractions of training data to compute learning curve
+    cv_learn = []
+    train_learn = []
+    n_train_array = []
+    # shuffle indices
+    ix_train_all = np.random.choice(len(train_class), len(train_class),
+                                    replace=False)
+    for i_f in range(n_learning_curve):
+        n_train = int(len(train_class) * (i_f+1) / float(n_learning_curve))
+        ix_train = ix_train_all[:n_train]
+        if 1 in train_class[ix_train]: # require at least 1 preictal case
+            n_train_array.append(n_train)
+            model.fit(train_features[ix_train], train_class[ix_train])
+            cv_learn.append(roc_auc_score(cv_class,
+                                          model.predict_proba( \
+                                              cv_features)[:,1]))
+            train_learn.append(roc_auc_score(train_class[ix_train],
+                                             model.predict_proba( \
+                                                train_features[ix_train])[:,1]))
+    ax0.plot(n_train_array, train_learn, 'r-')
+    ax0.plot(n_train_array, cv_learn, 'k-')
+    
     # train the model
     model.fit(train_features, train_class)
     print 'Feature coefficients:', model.coef_
@@ -53,14 +78,21 @@ for i_cv in range(n_cv):
     # get classification probabilities, plot ROC curve, and compute AUC
     p_pre = model.predict_proba(cv_features)[:,1]
     fp_rate, tp_rate, thresholds = roc_curve(cv_class, p_pre)
-    plt.plot(fp_rate, tp_rate, 'k-')
+    ax1.plot(fp_rate, tp_rate, 'k-')
     auc = roc_auc_score(cv_class, p_pre)
     print 'AUC =', auc
     auc_values.append(auc)
 
+    # compute AUC for training sample
+    p_pre_train = model.predict_proba(train_features)[:,1]
+    auc_train = roc_auc_score(train_class, p_pre_train)
+    print 'training AUC =', auc_train
+    
 print '\n Average AUC:'
 print np.mean(auc_values), '+/-', np.std(auc_values)
-    
-plt.xlabel('false positive rate')
-plt.ylabel('true positive rate')
+
+ax0.set_xlabel('number of training instances')
+ax0.set_ylabel('AUC') 
+ax1.set_xlabel('false positive rate')
+ax1.set_ylabel('true positive rate')
 plt.show()
