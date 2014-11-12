82676cc8fdaa351373472077b5dcc11a72134f81
Michael Mortonson <michael.mortonson@gmail.com>
2014-11-12 13:26:25 -0800

    Add power spectrum features (mean, cov. in freq. bins)

--- features.py
+++ features.py
@@ -19,7 +19,46 @@ def std_of_stds(data):
 def max_dev(data):
     return np.max(np.abs(data.T - np.mean(data, axis=1)))
 
-def compute_features(data, functions):
+def binned_power_spectrum(data, freq_bin_edges, sampling_rate):
+    freq = np.fft.rfftfreq(data.size, d=1./sampling_rate)
+    power = np.abs(np.fft.rfft(data))**2
+    return np.histogram(freq, freq_bin_edges, weights=power)[0] / \
+           np.histogram(freq, freq_bin_edges)[0]
+                       
+def power_mean(data, sampling_rate_hz):
+    # define frequency bins (units = Hz)
+    freq_bin_edges = np.logspace(-2, 2, num=6)
+
+    # compute binned power spectrum for each electrode
+    power = np.zeros(len(freq_bin_edges)-1)
+    for i in range(data.shape[0]):
+        power_i = binned_power_spectrum(data[i,:], freq_bin_edges,
+                                        sampling_rate_hz)
+        power = np.vstack((power, power_i))
+    power = power[1:,:]
+
+    # compute the mean in each frequency bin
+    return np.mean(power, axis=0)
+
+def power_cov(data, sampling_rate_hz):
+    # define frequency bins (units = Hz)
+    freq_bin_edges = np.logspace(-2, 2, num=6)
+
+    # compute binned power spectrum for each electrode
+    power = np.zeros(len(freq_bin_edges)-1)
+    for i in range(data.shape[0]):
+        power_i = binned_power_spectrum(data[i,:], freq_bin_edges,
+                                        sampling_rate_hz)
+        power = np.vstack((power, power_i))
+    power = power[1:,:]
+
+    # compute the covariance between frequency bins
+    cov = np.cov(power, rowvar=0)
+    # order of returned elements is 11, 12, ..., 1N, 22, 23, ..., NN
+    return cov[np.triu_indices(len(cov))]
+
+
+def compute_features(data, functions, function_args):
     """
     Given a matrix with EEG voltage time series in rows
     (one row for each electrode recorded in a particular segment),
@@ -27,21 +66,27 @@ def compute_features(data, functions):
     an array with the resulting features and a list of column labels.
     """
     features = []
-    for f in functions:
-        new_features = f(data)
+    for f, args in zip(functions, function_args):
+        if args is None:
+            new_features = f(data)
+        else:
+            new_features = f(data, *args)
         try:
             features.extend(new_features)
         except:
             features.append(new_features)
     return features
 
-def compute_feature_matrix(data_dir, functions, labels, save_file=None):
+def compute_feature_matrix(data_dir, functions, function_args, labels,
+                           save_file=None):
     """
     For each .mat EEG data file in data_dir, compute the features given
     by functions and labels and return a 2D array where each row contains
     the index of the hour the segment belongs to, the segment type
     ('preictal': 1, 'interictal': 0, 'test': -1), and its features.
     Save the resulting feature matrix if the save_file keyword is set.
+    Additional arguments required to evaluate function should be passed
+    in function_args (use None for functions that need no extra arguments).
     """
     X = np.zeros(len(labels) + 2) # add 2 columns for hour and type
     data_files = []
@@ -50,7 +95,8 @@ def compute_feature_matrix(data_dir, functions, labels, save_file=None):
         if f.split('.')[-1] == 'mat':
             data_files.append(f)
             data = load_data.load_data(os.path.join(data_dir, f))
-            new_features = compute_features(data['data'], functions)
+            new_features = compute_features(data['data'], functions,
+                                            function_args)
             if data['type'] == 'preictal':
                 seg_type = 1
             elif data['type'] == 'interictal':
